8b58c34eaab5c00e1f7b112fa555151631786adb
Sean Tracey <seanmtracey@gmail.com>
2016-08-01 14:54:21 +0100

    Multiple images can now be compared to one image

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const denodeify = require('denodeify');
 
 const compare = require('./lib/compare');
 
-compare('./tmp/6.png', './tmp/7.png')
+compare.single('./tmp/n.jpg', './tmp/7.png')
 	.then(sim => {
 		console.log(`These two images are ${sim * 100}% similar.`);
+	})
+;
+
+console.time('many');
+compare.many('./tmp/n.jpg', ['./tmp/7.png', './tmp/12.png', './tmp/6.png', './tmp/m.png'])
+	.then(res => {
+		console.timeEnd('many');
 	})
 ;
