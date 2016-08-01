17dec9cf8a6eb0d1b24d09be96b06ad725e0d438
Sean Tracey <seanmtracey@gmail.com>
2016-08-01 15:32:58 +0100

    Many example

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const denodeify = require('denodeify');
 
 const compare = require('./lib/compare');
 
-compare.single('./tmp/n.jpg', './tmp/7.png')
+compare.single('./tmp/n.jpg', './tmp/blank.png')
 	.then(result => {
 		// console.log(`These two images are ${sim * 100}% similar.`);
 		console.log(`${result.a} is ${result.similarity * 100}% similar to ${result.b}`);
 	})
 ;
 
-compare.many('./tmp/n.jpg', [ './tmp/12.png', './tmp/7.png', './tmp/6.png', './tmp/m.png'], true)
+console.time('many');
+compare.many('./tmp/n.jpg', [ './tmp/12.png', './tmp/7.png', './tmp/6.png', './tmp/m.png', './tmp/n.jpg'], true)
 	.then(res => {
-		console.log(res);
+		console.timeEnd('many');
+		res.forEach(comparison =>{
+			console.log(`${comparison.a} is ${comparison.similarity * 100}% similar to ${comparison.b}`);
+		});
+
 	})
 	.catch(err => {
 		console.log(err);
