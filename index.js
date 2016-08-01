7b45cdc60538eda75a24af1e567e4eb4580bb65a
Sean Tracey <seanmtracey@gmail.com>
2016-08-01 15:28:59 +0100

    (let)s put the consts where they belong (see what I did there?)

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const denodeify = require('denodeify');
 const compare = require('./lib/compare');
 
 compare.single('./tmp/n.jpg', './tmp/7.png')
-	.then(sim => {
-		console.log(`These two images are ${sim * 100}% similar.`);
+	.then(result => {
+		// console.log(`These two images are ${sim * 100}% similar.`);
+		console.log(`${result.a} is ${result.similarity * 100}% similar to ${result.b}`);
 	})
 ;
 
-console.time('many');
-compare.many('./tmp/n.jpg', ['./tmp/7.png', './tmp/12.png', './tmp/6.png', './tmp/m.png'])
+compare.many('./tmp/n.jpg', [ './tmp/12.png', './tmp/7.png', './tmp/6.png', './tmp/m.png'], true)
 	.then(res => {
-		console.timeEnd('many');
 		console.log(res);
 	})
 	.catch(err => {
