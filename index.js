c31d19bfd736e51ebcbd049b6730f7a7bea410e1
Sean Tracey <seanmtracey@gmail.com>
2016-08-01 11:20:31 +0100

    Normalising process now uses JIMP

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 'use strict';
 
 const denodeify = require('denodeify');
-const getPixels = denodeify( require('get-pixels') );
+
 const compare = require('./lib/compare');
 
-compare('./tmp/a.png', './tmp/a.png');
-
-/*getPixels('./tmp/a.png').then(function(pixels) {
-  console.log("got pixels", pixels.data)
-});*/
+compare('./tmp/a.png', './tmp/7.png');
